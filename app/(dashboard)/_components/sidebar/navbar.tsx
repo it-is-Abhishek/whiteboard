0c95016380007f71c05b1bc3acf90da916bac462
it-is-Abhishek <abhishekverma1489@gmail.com>
2025-12-31 02:38:37 +0530

    fix: search input debounce and build issues

--- app/(dashboard)/_components/sidebar/navbar.tsx
+++ app/(dashboard)/_components/sidebar/navbar.tsx
@@ -1,12 +1,37 @@
 "use client"
+import { SearchInput } from "../search-input";
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 
-import { UserButton } from "@clerk/nextjs";
 
 export const Navbar = () => {
     return (
-        <div className="flex items-center gap-x-4 p-5 bg-green-500">
-            <div className="hidden lg:flex lg:flex-1 bg-yellow-500">
-                Search
+        <div className="flex items-center gap-x-4 p-5">
+            <div className="hidden lg:flex lg:flex-1">
+                <SearchInput/>
+                <div className="block lg:hidden flex-1">
+                    <OrganizationSwitcher
+                        hidePersonal
+                        appearance={{
+                        elements: {
+                        rootBox:{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                            maxWidth: "367px"
+                        },
+                        organizationSwitcherTrigger: {
+                            padding:"6px",
+                            width: "100%",
+                            borderRadius: "8px",
+                            border: "1px solid #E5E7EB",
+                            justifyContent: "space-between",
+                            backgroundColor: "white",
+                        }
+                    }
+                }}
+            />
+                </div>
             </div>
             <UserButton/>
         </div>
