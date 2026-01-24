68b738dd9ccf1852d2d342ad8451f62a234c305a
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-01-24 13:06:30 +0530

    feat(board): add actions dropdown with copy link

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 import { Footer } from "./footer";
+import { MoreHorizontal } from "lucide-react";
 import { Overlay } from "./overlay";
+import { Skeleton } from "@/components/ui/skeleton";
+import { Actions } from "@/components/actions";
 
 
 interface BoardCardProps {
@@ -47,6 +50,19 @@ export const BoardCard = ({
             className="object-cover"
           />
           <Overlay/>
+          <Actions
+            id = {id}
+            title={title}
+            side = "right"
+          >
+            <button
+              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+            >
+              <MoreHorizontal
+                className="text-white opacity-75 hover:opacity-100 transition-opacity"
+              />
+            </button>
+          </Actions>
         </div>
         <Footer
             isFavorite = {isFavorite}
@@ -58,5 +74,13 @@ export const BoardCard = ({
         />
       </div>
     </Link>
+  );
+};
+
+BoardCard.Skeleton = function BoardCardSkeleton(){
+  return (
+    <div className="aspect-[100/127] rounded-lg overflow-hidden">
+      <Skeleton className="h-full w-full"/>
+    </div>
   );
 };
--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -25,10 +25,19 @@ export const BoardList = ({
     if (data === undefined){
         return (
             <div>
-                Loading...
+                <h2 className="text-3xl">
+                    {query.favorites ? "Favorite Boards" : "Team Boards"}
+               </h2>
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-5 mt-8 pb-10">
+                <NewBoardButton orgId={orgId} disabled />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                <BoardCard.Skeleton />
+                </div>
             </div>
-        )
-    }
+        );
+    };
 
     if (!data?.length && query.search) {
         return <EmptySearch/>
