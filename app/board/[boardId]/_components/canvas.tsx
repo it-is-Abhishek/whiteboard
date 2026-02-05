28e9c4d3deafc13c35fc5977d8679fa4dd0f75fc
it-is-Abhishek <abhishekverma1489@gmail.com>
2026-02-05 13:22:18 +0530

    feat: implement toolbar tool selection and undo/redo logic

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,12 @@
 "use client";
 
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 import { Info} from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { set } from "date-fns";
+import { useHistory, useCanRedo , useCanUndo} from "@liveblocks/react";
 
 interface Canvasprops {
     boardId: string;
@@ -12,12 +16,28 @@ export const Canvas = ({
     boardId,
 }: Canvasprops) => {
 
+    const [canvasState, setCanvasState] = useState<CanvasState>({
+        mode: CanvasMode.None,
+    });
+
+    const history = useHistory();
+    const canUndo = useCanUndo();
+    const canRedo = useCanRedo();
+
+
     return (
         <main
         className="h-full w-full relaive bg-neutral-100 touch-none">
             <Info boardId={boardId}/>
             <Participants/>
-            <Toolbar/>
+            <Toolbar
+                canvasState={canvasState}
+                setCanvasState={setCanvasState}
+                canRedo = {canRedo}
+                canUndo = {canUndo}
+                undo = {history.undo}
+                redo = {history.redo}
+            />
         </main>
     );
 };
